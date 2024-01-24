3e4600dd56e7a6b3f4330ba4da1ce676953868c6
Sandeep Kumar M <sdagangstar11@gmail.com>
2024-01-24 11:21:08 +0530

    container1 responsive for mobile

--- lib/screens/containers/container1.dart
+++ lib/screens/containers/container1.dart
@@ -28,16 +28,16 @@ class _Container1State extends State<Container1> {
                   style: TextStyle(
                     fontSize: w! / 20,
                     fontWeight: FontWeight.bold,
-                    height:1,
+                    height: 1,
                   ),
                 ),
                 const SizedBox(
                   height: 10,
                 ),
-                Text(
+                const Text(
                   "Helps you to organize your income and expenses",
                   style: TextStyle(
-                    fontSize: w! / 70,
+                    fontSize: 16,
                     color: Colors.grey,
                   ),
                 ),
@@ -73,10 +73,10 @@ class _Container1State extends State<Container1> {
                     const SizedBox(
                       width: 20,
                     ),
-                    Text(
+                    const Text(
                       "— Web, iOs and Android",
                       style: TextStyle(
-                        fontSize: w! / 70,
+                        fontSize: 16,
                         color: Colors.grey,
                       ),
                     ),
@@ -100,7 +100,96 @@ class _Container1State extends State<Container1> {
       );
     }
 
+    Widget mobileContainer1(BuildContext context) {
+      return Container(
+        margin: EdgeInsets.symmetric(horizontal: w! / 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: w! / 1.2,
+              width: w! / 1.2,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(illustration1),
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Track your Expenses \nto Save Money",
+              style: TextStyle(
+                fontSize: w! / 12,
+                fontWeight: FontWeight.bold,
+                height: 1,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "Helps you to organize your income and expenses",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Column(
+              children: [
+                Container(
+                  height: 45,
+                  child: ElevatedButton.icon(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      elevation: MaterialStateProperty.all(0),
+                      backgroundColor:
+                          MaterialStateProperty.all(AppColor.primary),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    icon: const Icon(
+                      Icons.arrow_drop_down,
+                      color: Colors.white,
+                    ),
+                    label: const Text(
+                      "Try Free Demo",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "— Web, iOs and Android",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
     return ScreenTypeLayout.builder(
+      mobile: mobileContainer1,
       desktop: desktopContainer1,
     );
   }

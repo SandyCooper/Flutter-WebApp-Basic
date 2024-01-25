852162ca618d271673a776cf1d17343e8337ac31
Sandeep Kumar M <sdagangstar11@gmail.com>
2024-01-25 12:25:42 +0530

    developing container2 in mobiile responsive

--- lib/screens/containers/container2.dart
+++ lib/screens/containers/container2.dart
@@ -93,96 +93,59 @@ class _Container2State extends State<Container2> {
       );
     }
 
-    // Widget mobileContainer2(BuildContext context) {
-    //   return Container(
-    //     margin: EdgeInsets.symmetric(horizontal: w! / 10),
-    //     child: Column(
-    //       mainAxisAlignment: MainAxisAlignment.center,
-    //       // crossAxisAlignment: CrossAxisAlignment.start,
-    //       children: [
-    //         Container(
-    //           height: w! / 1.2,
-    //           width: w! / 1.2,
-    //           decoration: const BoxDecoration(
-    //             image: DecorationImage(
-    //               image: AssetImage(illustration1),
-    //               fit: BoxFit.contain,
-    //             ),
-    //           ),
-    //         ),
-    //         const SizedBox(
-    //           height: 20,
-    //         ),
-    //         Text(
-    //           "Track your Expenses \nto Save Money",
-    //           style: TextStyle(
-    //             fontSize: w! / 12,
-    //             fontWeight: FontWeight.bold,
-    //             height: 1,
-    //           ),
-    //         ),
-    //         const SizedBox(
-    //           height: 10,
-    //         ),
-    //         const Text(
-    //           "Helps you to organize your income and expenses",
-    //           textAlign: TextAlign.center,
-    //           style: TextStyle(
-    //             fontSize: 16,
-    //             color: Colors.grey,
-    //           ),
-    //         ),
-    //         const SizedBox(
-    //           height: 20,
-    //         ),
-    //         Column(
-    //           children: [
-    //             Container(
-    //               height: 45,
-    //               child: ElevatedButton.icon(
-    //                 onPressed: () {},
-    //                 style: ButtonStyle(
-    //                   elevation: MaterialStateProperty.all(0),
-    //                   backgroundColor:
-    //                       MaterialStateProperty.all(AppColor.primary),
-    //                   shape: MaterialStateProperty.all(
-    //                     RoundedRectangleBorder(
-    //                       borderRadius: BorderRadius.circular(10),
-    //                     ),
-    //                   ),
-    //                 ),
-    //                 icon: const Icon(
-    //                   Icons.arrow_drop_down,
-    //                   color: Colors.white,
-    //                 ),
-    //                 label: const Text(
-    //                   "Try Free Demo",
-    //                   style: TextStyle(color: Colors.white),
-    //                 ),
-    //               ),
-    //             ),
-    //             const SizedBox(
-    //               height: 20,
-    //             ),
-    //             const Text(
-    //               "— Web, iOs and Android",
-    //               style: TextStyle(
-    //                 fontSize: 16,
-    //                 color: Colors.grey,
-    //               ),
-    //             ),
-    //             const SizedBox(
-    //               height: 20,
-    //             ),
-    //           ],
-    //         ),
-    //       ],
-    //     ),
-    //   );
-    // }
+    Widget mobileContainer2(BuildContext context) {
+      return Container(
+        // padding: const EdgeInsets.only(top: 40),
+        width: double.infinity,
+        decoration: BoxDecoration(color: AppColor.primary),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 20,
+                top: 20,
+                bottom: 0,
+              ),
+              child: Container(
+                height: 195,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(dashboard),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [fb, google, cocacola, samsung]
+                      .map(
+                        (e) => Container(
+                          height: 20,
+                          width: 100,
+                          margin: const EdgeInsets.symmetric(
+                            vertical: 10,
+                          ),
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(e),
+                            ),
+                          ),
+                        ),
+                      )
+                      .toList()),
+            ),
+          ],
+        ),
+      );
+    }
 
     return ScreenTypeLayout.builder(
-      // mobile: mobileContainer2,
+      mobile: mobileContainer2,
       desktop: desktopContainer2,
     );
   }
